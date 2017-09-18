0a612f75110f03318958f29ef83d3b9de0a08dde
Dana <Dana@Dana-PC.computer.Carter-PC.com>
2017-09-17 20:27:28 -0700

    Started interface

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import javax.swing.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-
+		JFrame main = new JFrame("Cubic Equation Generator");
+		JOptionPane.showMessageDialog(null, "Fill this out.");
 	}
 
 }
