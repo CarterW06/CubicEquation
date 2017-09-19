68987f40e735764067871d810e4e7369f3655c7a
Dana <Dana@Dana-PC.computer.Carter-PC.com>
2017-09-19 14:31:23 -0700

    Added most variables

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
+import java.util.Random;
+
 import javax.swing.*;
 
 public class Main {
 
 	public static void main(String[] args) {
+		int a = new Random().nextInt(9);
+		int b = new Random().nextInt(9);
+		int c = new Random().nextInt(9);
+		int d = new Random().nextInt(9);
+		int e = new Random().nextInt(9);
 		JFrame main = new JFrame("Cubic Equation Generator");
-		JOptionPane.showMessageDialog(null, "Fill this out.");
+		JPanel jp = new JPanel();
+		JLabel text = new JLabel("Please enter the solutions to the following cubic equation.  If there are several solutions that are the same, enter the solution several times.");
 	}
 
 }
