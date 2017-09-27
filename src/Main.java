bedda2975533fc2efe7f2c733a744f4a092d670b
Dana <Dana@Dana-PC.computer.Carter-PC.com>
2017-09-27 16:19:44 -0700

    Added to UI

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,21 @@
-import java.util.Random;
-
 import javax.swing.*;
 
 public class Main {
 
+	static JPanel jp1 = new JPanel();
 	public static void main(String[] args) {
-		int a = new Random().nextInt(9);
-		int b = new Random().nextInt(9);
-		int c = new Random().nextInt(9);
-		int d = new Random().nextInt(9);
-		int e = new Random().nextInt(9);
+		JPanel jp2 = new JPanel();
 		JFrame main = new JFrame("Cubic Equation Generator");
-		JPanel jp = new JPanel();
 		JLabel text = new JLabel("Please enter the solutions to the following cubic equation.  If there are several solutions that are the same, enter the solution several times.");
+		JLabel cubic = new JLabel(generateCubic());
+		JTextField answer = new JTextField();
+		JButton check = new JButton("Check your answer");
+		jp1.add(text);
+		
+	}
+	
+	private static String generateCubic() {
+		return "";
 	}
 
 }
